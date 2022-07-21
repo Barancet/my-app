626f566f052788730cdcdc78064e6492d21d9067
barancet <barcet95@gmail.com>
2022-07-21 15:03:38 -0400

    first stages of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+// const Header = (props) => {
+//   return <h1>{props.course}</h1>;
+// };
+
+// const Part = (props) => {
+//   return (
+//     <p>
+//       {props.part} {props.exercise}
+//     </p>
+//   );
+// };
+
+// const Content = (props) => {
+//   return (
+//     <div>
+//       <ul>
+//         <li>
+//           <Part
+//             part={props.parts[0].name}
+//             exercise={props.parts[0].exercises}
+//           />
+//         </li>
+//         <li>
+//           <Part
+//             part={props.parts[1].name}
+//             exercise={props.parts[1].exercises}
+//           />
+//         </li>
+//         <li>
+//           <Part
+//             part={props.parts[2].name}
+//             exercise={props.parts[2].exercises}
+//           />
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const Total = (props) => {
+//   return (
+//     <p>
+//       num of exercises{" "}
+//       {props.parts[0].exercises +
+//         props.parts[1].exercises +
+//         props.parts[2].exercises}
+//     </p>
+//   );
+// };
+
+// const Hello = ({name, age}) => {
+
+//   const birthYear = () => {
+//     const currentYear = new Date().getFullYear();
+//     console.log(currentYear);
+//     return currentYear - age;
+//   };
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name} you are {age} years old.
+//       </p>
+//       <p>so that means you were probably born in {birthYear()}</p>
+//     </div>
+//   );
+// };
+
+// const App = (props) => {
+
+//   const {counter} = props
+//   const course = {
+//     name: "Half Stack application development",
+//     parts: [
+//       {
+//         name: "Fundamentals of React",
+//         exercises: 10,
+//       },
+//       {
+//         name: "Using props to pass data",
+//         exercises: 7,
+//       },
+//       {
+//         name: "State of a component",
+//         exercises: 14,
+//       },
+//     ],
+//   };
+
+//   return (
+//     <div>
+//       <Header course={course.name} />
+//       <Content parts={course.parts} />
+//       <Total parts={course.parts} />
+//       <Hello name="baran" age="27" />
+//       <div>{counter}</div>
+//     </div>
+//   );
+// };
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  console.log("rendering...", counter);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+let counter = 1;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*lesson 1 Default React Template for index.js*/
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App counter={counter} />
+//   </React.StrictMode>
+// );
+/*Lesson 2 with non ideal way to handle rerendering of the page */
+// const refresh = () => {
+//   ReactDOM.createRoot(document.getElementById("root")).render(
+//     <App counter={counter} />
+//   );
+// };
 
+// setInterval(() => {
+//   refresh();
+//   counter += 1;
+// }, 1000);
+
+/** Lesson 3 with proper state re rendering  */
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+reportWebVitals(console.log);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
