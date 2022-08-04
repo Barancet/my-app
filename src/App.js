9276de1cff182857a4013cc00312b6b659f97af1
barancet <barcet95@gmail.com>
2022-08-04 15:31:29 -0400

    changes to make it work

--- src/App.js
+++ src/App.js
@@ -148,42 +148,48 @@ const History = (props) =>{
   )
 }
 
-const Button = ({ handleClick, text }) => {
+const Button = ({ handleClick, text }) => 
   <button onClick={handleClick}>
     {text}
   </button>
-}
+
 
 const App = () => {
   //maintain clicks
-  // const [clicks, setClicks] = useState({
-  //   left: 0, right: 0
-  // })
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
 
-  // const handleLeftClick = () => 
-  //   setClicks({ ...clicks, left: clicks.left+1})
-  // const handleRightClick = () =>
-  //   setClicks( { ...clicks, right: clicks.right+1})
-
   const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left+1})
     setAll(allClicks.concat('L'))
-    setLeft(left+1)
   }
+
   const handleRightClick = () => {
-    setAll(allClicks.concat("R"))
-    setRight(right+1)
+    setClicks( { ...clicks, right: clicks.right+1})
+    setAll(allClicks.concat('R'))
   }
+    
+
+  // const handleLeftClick = () => {
+  //   setAll(allClicks.concat('L'))
+  //   setLeft(left+1)
+  // }
+  // const handleRightClick = () => {
+  //   setAll(allClicks.concat("R"))
+  //   setRight(right+1)
+  // }
 
   return(
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text="left"/>
-      <Button onClick={handleRightClick} text="Right"/>
-      {right}
+      {clicks.left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="Right" />
+      {clicks.right}
       <History allClicks={allClicks} />
     </div>
   )
